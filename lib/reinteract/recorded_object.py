1a6c2a596647345662697f85f091db4757b86da0
Owen W. Taylor <otaylor@fishsoup.net>
2010-01-20 07:50:39 -0500

    Improve argument checking in RecordedObject
    Handle keyword arguments when checking the supplied arguments; previously
    we only handled the case of a function with '**kwargs', and didn't handle
    the case of supplying kwargs for a functions defined arguments.
    
    Add comprehensive tests on the argument checking logic.

--- lib/reinteract/recorded_object.py
+++ lib/reinteract/recorded_object.py
@@ -40,6 +40,13 @@ class ReplayException(Exception):
                            'exc_desc': str(self.orig_exception) }
         return exc_string
 
+def _arg_count(n, non_keyword=False):
+    non_keyword_s = 'non-keyword ' if non_keyword else ''
+    if n == 1:
+        return "1 %sargument" % (non_keyword_s,)
+    else:
+        return "%d %sarguments" % (n, non_keyword_s)
+
 class RecordedObject(object):
     """
     A RecordedObject is a proxy for another object that Reinteract can't copy
@@ -74,34 +81,51 @@ class RecordedObject(object):
 
     def _check_call(self, name, args, kwargs, spec):
         # This tries to duplicate some of python's argument checking logic
-        num_args     = len(spec[0]) if spec[0] else 0
-        use_varargs  = True if spec[1] else False
-        use_kwargs   = True if spec[2] else False
-        num_defaults = len(spec[3]) if spec[3] else 0
+        num_args      = len(spec[0]) if spec[0] else 0
+        use_varargs   = True if spec[1] else False
+        use_varkwargs = True if spec[2] else False
+        num_defaults  = len(spec[3]) if spec[3] else 0
 
         given_args = len(args) + 1 # self not included
         min_args = num_args - num_defaults
+
+        if kwargs:
+            for k, v in kwargs.iteritems():
+                try:
+                    pos = spec[0].index(k)
+                except ValueError, e:
+                    pos = -1
+
+                if pos >= 0:
+                    if pos < len(args) + 1:
+                        # Duplicate of an argument specified without a keyword
+                        raise TypeError("%(name)s() got multiple values for keyword argument '%(kw)s'" % {
+                              'name': name,
+                              'kw': k })
+                    elif pos < num_args - num_defaults:
+                        # If the user names a required argument, we don't need to look for it in args
+                        given_args += 1
+                elif not use_varkwargs:
+                    raise TypeError("%(name)s() got an unexpected keyword argument '%(kw)s'" % {
+                         'name': name,
+                         'kw': k })
+
         exact_args = num_defaults == 0 and not use_varargs
-        if exact_args and given_args != num_args:
-            raise TypeError("%(name)s() takes exactly %(reqd)d arguments (%(nargs)d given)" % {
+        if exact_args and given_args != min_args:
+            raise TypeError("%(name)s() takes exactly %(reqd)s (%(nargs)d given)" % {
                 'name': name,
-                'reqd': num_args,
+                'reqd': _arg_count(num_args),
                 'nargs': given_args })
         elif given_args < min_args:
-            raise TypeError("%(name)s() takes at least %(reqd)d arguments (%(nargs)d given)" % {
+            raise TypeError("%(name)s() takes at least %(reqd)s (%(nargs)d given)" % {
                 'name': name,
-                'reqd': min_args,
+                'reqd': _arg_count(min_args, non_keyword=kwargs),
                 'nargs': given_args })
         elif given_args > num_args and not use_varargs:
-            raise TypeError("%(name)s() takes at most %(reqd)d arguments (%(nargs)d given)" % {
+            raise TypeError("%(name)s() takes at most %(reqd)s (%(nargs)d given)" % {
                 'name': name,
-                'reqd': num_args,
+                'reqd': _arg_count(num_args),
                 'nargs': given_args })
-
-        if kwargs and not use_kwargs:
-            raise TypeError("%(name)s() got an unexpected keyword argument '%(kw)s'" % {
-                'name': name,
-                'kw': kwargs.iterkeys().next()})
 
     @classmethod
     def _set_target_class(cls, baseclass, attr_filter=default_filter):
@@ -139,3 +163,71 @@ class RecordedObject(object):
             record.__doc__ = getattr(baseclass, attr).__doc__
             setattr(cls, attr, record)
 
+if __name__ == '__main__': #pragma: no cover
+    from test_utils import assert_equals
+
+    class TestTarget:
+        def __init__():
+            pass
+
+        def exactargs(self, a, b):
+            pass
+
+        def defaultargs(self, a, b=1):
+            pass
+
+        def varargs(self, *args):
+            pass
+
+        def kwargs(self, **kwargs):
+            pass
+
+    class TestRecorded(RecordedObject):
+        pass
+
+    TestRecorded._set_target_class(TestTarget)
+    o = TestRecorded()
+
+    # Tests of our argument checking
+
+    def expect_ok(method, *args, **kwargs):
+        o.__class__.__dict__[method](o, *args, **kwargs)
+
+    def expect_fail(method, msg, *args, **kwargs):
+        try:
+            o.__class__.__dict__[method](o, *args, **kwargs)
+            raise AssertionError("Expected failure with '%s', got success" % (msg,))
+        except TypeError, e:
+            if str(e) != msg:
+                raise AssertionError("Expected failure with '%s', got '%s'" % (msg, str(e)))
+
+    expect_ok('exactargs', 1, 2)
+    expect_ok('exactargs', 1, b=2)
+    expect_fail('exactargs', "exactargs() takes exactly 3 arguments (2 given)",
+                1)
+    expect_fail('exactargs', "exactargs() got an unexpected keyword argument 'c'",
+                1, 2, c=3)
+    expect_fail('exactargs', "exactargs() got multiple values for keyword argument 'a'",
+                1, a=1)
+
+    expect_ok('defaultargs', 1, 2)
+    expect_ok('defaultargs', 1)
+    expect_ok('defaultargs', a=1, b=2)
+    expect_fail('defaultargs', "defaultargs() takes at least 2 arguments (1 given)",
+                )
+    expect_fail('defaultargs', "defaultargs() takes at least 2 non-keyword arguments (1 given)",
+                b=1)
+    expect_fail('defaultargs', "defaultargs() takes at most 3 arguments (4 given)",
+                1, 2, 3)
+    expect_fail('defaultargs', "defaultargs() got an unexpected keyword argument 'c'",
+                1, 2, c=3)
+    expect_fail('defaultargs', "defaultargs() got multiple values for keyword argument 'a'",
+                1, a=1)
+
+    expect_ok('varargs', 1)
+    expect_fail('varargs', "varargs() got an unexpected keyword argument 'a'",
+                1, a=1)
+
+    expect_ok('kwargs', a=1)
+    expect_fail('kwargs', "kwargs() takes exactly 1 argument (2 given)",
+                1)
